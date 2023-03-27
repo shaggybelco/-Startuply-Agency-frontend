e3d4342d88ba93737e161251d48614f8a3400667
shaggybelco <shaggybelco1@gmail.com>
2023-03-27 12:27:39 +0200

    reviews page

--- src/Pages/Review_sec/Review_sec.tsx
+++ src/Pages/Review_sec/Review_sec.tsx
@@ -1,19 +1,19 @@
 import './Review.scss';
-import customer from '../../assets/customer.svg';
-import world from '../../assets/world.svg';
+import teach from '../../assets/teach.svg';
+import bubble from '../../assets/bubble.svg';
 
 function Review_sec() {
     return (
         <div className='r_container'>
             <div className='rf'>
                 <div className='rf_2'>
-                    <img src={world} alt='none' />
+                    <img src={teach} alt='none' />
                     <h3>Automate customer reviews, referrals</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque illo provident nulla fugit fuga aut assumenda numquam praesentium expedita earum error quia minima atque et, aspernatur delectus veritatis dolor accusamus.</p>
-                    <div> How it works</div>
+                    <div> Get started</div>
                 </div>
                 <div className='rf_1'>
-                    <img src={customer} alt='none' />
+                    <img src={bubble} alt='none' />
                 </div>
 
             </div>
