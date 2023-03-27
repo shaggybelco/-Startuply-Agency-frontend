d2aca001094571d9b89bf5007aa1d399a6317595
shaggybelco <shaggybelco1@gmail.com>
2023-03-27 13:34:49 +0200

    reviews

--- src/Pages/Review_sec/Review_sec.tsx
+++ src/Pages/Review_sec/Review_sec.tsx
@@ -7,13 +7,13 @@ function Review_sec() {
         <div className='r_container'>
             <div className='rf'>
                 <div className='rf_2'>
-                    <img src={teach} alt='none' />
+                    <img src={bubble} alt='none' />
                     <h3>Automate customer reviews, referrals</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque illo provident nulla fugit fuga aut assumenda numquam praesentium expedita earum error quia minima atque et, aspernatur delectus veritatis dolor accusamus.</p>
                     <div> Get started</div>
                 </div>
                 <div className='rf_1'>
-                    <img src={bubble} alt='none' />
+                    <img src={teach} alt='none' />
                 </div>
 
             </div>
